f63cccd8172f7b7ed8597df21182d40dd4174b9e
shoehead0125 <shoehead0125@gmail.com>
2017-03-21 15:11:42 +0900

    Add image column in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318085713) do
+ActiveRecord::Schema.define(version: 20170321051036) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.string   "email",                                default: "", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170318085713) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
