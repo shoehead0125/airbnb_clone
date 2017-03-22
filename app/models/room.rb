ff9f2c2c57fa6baefde3246424548c4e4cb0c85c
shoehead0125 <shoehead0125@gmail.com>
2017-03-22 18:29:19 +0900

    Revise model.rb

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ApplicationRecord
+  mount_uploader :image, ImageUploader
   enum category: { reserved: 0, privated: 1, shared: 2 }
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  enum status: { normal: 0, host: 1 }
   mount_uploader :image, ImageUploader
+  enum status: { normal: 0, host: 1 }
   has_many :rooms
 end
