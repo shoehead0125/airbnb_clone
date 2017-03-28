2a19cff8158d3c30ee5ba02307ff4419cbd41cd3
shoehead0125 <shoehead0125@gmail.com>
2017-03-28 16:05:06 +0900

    Add column latitude, longitude to rooms table

--- db/migrate/20170322064205_add_column_to_room.rb
+++ db/migrate/20170322064205_add_column_to_room.rb
@@ -2,5 +2,7 @@ class AddColumnToRoom < ActiveRecord::Migration[5.0]
   def change
     add_column :rooms, :name, :string
     add_column :rooms, :image, :string
+    add_column :rooms, :latitude, :float
+    add_column :rooms, :longitude, :float
   end
 end
