67ad69bc5396a0c5b85b39542f4b786918f7564c
shoehead0125 <shoehead0125@gmail.com>
2017-04-02 15:22:22 +0900

    Set S3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,23 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_ACCESS_SECRET'],
+    region:                'ap-northeast-1',
+  }
+  config.storage = :fog
+  config.fog_public     = false
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+
+  case Rails.env
+    when 'production'
+      config.fog_directory = 'pro-airbnb-image-store'
+      config.asset_host = 'https://pro-airbnb-image-store.s3-ap-northeast-1.amazonaws.com'
+    when 'development'
+      config.fog_directory = 'dev-airbnb-image-store'
+      config.asset_host = 'https://dev-airbnb-image-store.s3-ap-northeast-1.amazonaws.com'
+    end
+end
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
