c9ad49b119b4aea431669d947c0e769c05b02bd9
shoehead0125 <shoehead0125@gmail.com>
2017-04-04 16:18:26 +0900

    Associate tables

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
+  has_many :resavations
   enum category: { reserved: 0, privated: 1, shared: 2 }
   geocoded_by :adress
   after_validation :geocode, if: :adress_changed?
