1d3ee0acd07600cc88a1c9d9bb03922d002e8489
shoehead0125 <shoehead0125@gmail.com>
2017-04-08 15:08:27 +0900

    Add show, submit action on resavations_controller

--- app/controllers/resavations_controller.rb
+++ app/controllers/resavations_controller.rb
@@ -8,6 +8,22 @@ class ResavationsController < ApplicationController
     end
   end
 
+  def show
+    @resavation = Resavation.find(params[:id])
+    @message = Message.new
+    @sum_price_of_stay = @resavation.room.price * @resavation.days
+  end
+
+  def submit
+    @resavation = Resavation.find(params[:id])
+    @resavation.status = 1
+    if @resavation.save
+      redirect_to resavation_path
+    else
+      render action: :show
+    end
+  end
+
   private
     def resavation_params
       params.require(:resavation).permit(:room_id, :start_day, :end_day, :occupancy, :price).merge(user_id: current_user.id)
