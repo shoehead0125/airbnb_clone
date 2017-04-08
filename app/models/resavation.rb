cc99467073aa8ec79d83a27cfba393863055f7e5
shoehead0125 <shoehead0125@gmail.com>
2017-04-08 14:50:00 +0900

    Associate models

--- app/models/resavation.rb
+++ app/models/resavation.rb
@@ -1,5 +1,11 @@
 class Resavation < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  has_many :messages
   enum status: { request: 0, confirm: 1 }
+
+  def days
+    days = self.end_day - self.start_day
+    return days.to_i
+  end
 end
