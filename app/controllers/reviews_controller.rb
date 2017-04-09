285dc3e384bf828350e58e65b56128618933827b
shoehead0125 <shoehead0125@gmail.com>
2017-04-09 16:15:20 +0900

    Add new and create action in reviews_controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @resavation = Resavation.find(params[:resavation_id])
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to resavations_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:body, :rate).merge(room_id: params[:room_id], user_id: current_user.id)
+  end
+end
