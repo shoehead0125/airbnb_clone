16b025dac7b476ef1cb678b2224b303afa66f514
shoehead0125 <shoehead0125@gmail.com>
2017-04-13 20:25:10 +0900

    Make bulk_create function

--- app/controllers/resavations_controller.rb
+++ app/controllers/resavations_controller.rb
@@ -12,6 +12,21 @@ class ResavationsController < ApplicationController
     end
   end
 
+  def bulk_create
+    @resavations = []
+    params[:resavation].each_with_index do |params, i|
+      room = Room.find(params[1][:room_id])
+      check_in = modify_to_date(params[1][:start_day])
+      check_out = modify_to_date(params[1][:end_day])
+      stay_days = (check_out - check_in).to_i
+      sum_price = room.price * stay_days
+      sum = sum_price + room.cleaning + room.service
+      @resavation = Resavation.new(resavation_bulk_params(i, sum))
+      @resavation.save
+    end
+    redirect_to resavations_path
+  end
+
   def show
     @resavation = Resavation.find(params[:id])
     @message = Message.new
@@ -29,7 +44,16 @@ class ResavationsController < ApplicationController
   end
 
   private
+
     def resavation_params
       params.require(:resavation).permit(:room_id, :start_day, :end_day, :occupancy, :price).merge(user_id: current_user.id)
+    end
+
+    def resavation_bulk_params(i, sum)
+      params.require(:resavation).require(i.to_s).permit(:room_id, :start_day, :end_day, :occupancy).merge(user_id: current_user.id, price: sum)
+    end
+
+    def modify_to_date(str)
+      Date.parse(str)
     end
 end
