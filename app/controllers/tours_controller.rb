15a53bde9b5405d73729352bd744cdcacd1884e7
shoehead0125 <shoehead0125@gmail.com>
2017-04-24 15:32:08 +0900

    Add search action in tours controller

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,30 +1,39 @@
 class ToursController < ApplicationController
   def index
-    @rooms = Array.new(5).map{Array.new}
-    @keyword = []
-    @starts = []
-    @ends = []
-    @occupancys = []
-    gon.keyword = []
-    gon.latlng = Array.new(5).map{Array.new}
-    params[:form].each do |i, params|
-      break if params[:address] == ''
-      gon.keyword[i.to_i] = params[:address]
-      @starts.push(params[:start_day])
-      @ends.push(params[:end_day])
-      @occupancys.push(params[:occupancy])
-      @keyword.push(params[:address])
-      rooms = Room.near([params[:latitude], params[:longitude]], 2)
-      refine_rooms_by_date(i, rooms, params)
-    end
-    @rooms.each_with_index do |rooms, i|
-      rooms.each do |room|
-        gon.latlng[i].push(lat: room.latitude, lng: room.longitude)
-      end
-    end
+    @tours = current_user.tours
   end
 
   def new; end
+
+  def search
+    @tour = Tour.new(tour_params)
+      if @tour.save
+        @rooms = Array.new(5).map { [] }
+        @keyword = []
+        @starts = []
+        @ends = []
+        @occupancys = []
+        gon.keyword = []
+        gon.latlng = Array.new(5).map { [] }
+        params[:form].each do |i, params|
+          break if params[:address] == ''
+          gon.keyword[i.to_i] = params[:address]
+          @starts.push(params[:start_day])
+          @ends.push(params[:end_day])
+          @occupancys.push(params[:occupancy])
+          @keyword.push(params[:address])
+          rooms = Room.near([params[:latitude], params[:longitude]], 2)
+          refine_rooms_by_date(i, rooms, params)
+        end
+        @rooms.each_with_index do |rooms, i|
+          rooms.each do |room|
+            gon.latlng[i].push(lat: room.latitude, lng: room.longitude)
+          end
+        end
+      else
+        render action: :new
+      end
+    end
 
   private
 
@@ -40,5 +49,9 @@ class ToursController < ApplicationController
       end
       @rooms[i.to_i] << room if flag.zero?
     end
+  end
+
+  def tour_params
+    params.require(:tour).permit(:name).merge(user_id: current_user.id)
   end
 end
